1a04405181b2eeac2d01d95c03c521874b4269cf
jvtcl <joseph.tuccillo@colorado.edu>
2020-06-04 15:01:52 -0600

    cleanup boulder demo

--- code/demo_boulder2.jl
+++ code/demo_boulder2.jl
@@ -87,25 +87,16 @@ end
 
 A1 = reshape(A1, nrow(constraints_bg), nrow(constraints_trt))
 A1 = transpose(A1)
-# A1 = sparse(A1)
-# A1 = dropzeros(A1)
-
 #%%
 
 #%% Target unit identity matrix
 A2 = Matrix(I, nrow(constraints_bg), nrow(constraints_bg))
 #%%
 
-#%%
-# A1 = dropzeros(sparse(A1))
-# A2 = dropzeros(sparse(A2))
-#%%
-
 #%% Solution Space
 X1 = (sparse(pX') ⊗ A1)'
 X2 = (sparse(pX') ⊗ A2)'
 @time X = hcat(sparse(X1), sparse(X2));
-# X = dropzeros(X)
 #%%
 
 #%% clear intermediates
@@ -135,7 +126,7 @@ sV = Diagonal(V_vec)
 
 #%%
 compute_allocation = function(q, X, λ)
-    qXl = exp.(-X * λ) .* q
+    qXl = q .* exp.(-X * λ)
     qXl / sum(qXl)
 end
 #%%
@@ -154,30 +145,6 @@ f = function(λ)
 end
 #%%
 
-#%%
-# # @time neg_pe(λ)
-# init_λ = zeros(length(Y_vec))
-
-λ = zeros(length(Y_vec))
-# λ = sparse(λ);
-
-# # init_λ = repeat([0.], length(Y_vec))
-# # init_λ = Y_vec + (sV * λ) - Xp;
-# # init_λ = rand(length(Y_vec));
-# @time f(init_λ)
-#%%
-
-## making λ sparse is the only thing that seems to help time-wise
-# q = sparse(q);
-# X = dropzeros(X)
-@time f(λ)
-
-#%%
-# using ForwardDiff
-# # @time Y_vec + (sV * λ) - Xp
-# @time ForwardDiff.gradient(f, λ)
-#%%
-
 #%% Gradient function
 g! = function(G, λ)
     qXl = q .* exp.(-X * λ)
@@ -185,8 +152,6 @@ g! = function(G, λ)
     Xp = X'p
     G[:] = Y_vec + (sV * λ) - Xp
 end
-
-@time g!(zeros(length(λ)), λ)
 #%%
 
 #%% Hessian
@@ -198,53 +163,42 @@ h! = function(H, λ)
 end
 #%%
 
-# @time h!(Array{Float64}(undef, 1665, 1665), init_λ)
-
-# precond = function(n)
-#     spdiagm((-ones(n-1), 2*ones(n), -ones(n-1)), (-1,0,1), n, n)*(n+1)
-# end
-#
-# precond(1665)
-#
-# n = 1665
-# spdiagm((-ones(n-1), 2*ones(n), -ones(n-1)), (-1,0,1), n, n)*(n+1)
-#
-# precond(1665)
-#
-# spdiagm(-1 => [1,2,3,4], 1 => [4,3,2,1])
-
 #%%
 using Optim
 
 init_λ = zeros(length(Y_vec));
-# init_λ = rand(length(Y_vec))
 
-# SOME NOTES ON THE DIFFERENT SOLVERS
-#
-# - TRUST REGION is extremely slow - MUCH faster with precomputed Hessian
-#
-# - GRADIENT DESCENT does not converge at 1000 iterations
-#   but gives a reasonable solution (0.84% constraints unmatched)
+# - TRUST REGION needs Hessian function to run efficiently.
+#   With Hessian in hand, execution time is somewhat close to Rcpp version!
+#  Rcpp version ~22 sec; This version ~29 - 30sec.
 
 @time opt = optimize(f, g!, h!, init_λ, NewtonTrustRegion(),
                     Optim.Options(show_trace=true, iterations = 200))
-
-# @time opt = optimize(f, g!, init_λ, GradientDescent(),
-                    # Optim.Options(show_trace=true, iterations = 1000))
-
-# # SLOW!! ~3.74 hours
-# @time opt = optimize(f, init_λ, BFGS(), autodiff = :forward,
-#             Optim.Options(show_trace=true, iterations = 10)) # orig 100
-
-# final lambda
-λ = Optim.minimizer(opt);
 #%%
 
-# # dump results
-# CSV.write("data/boulder_run1.csv", DataFrame(lambda = λ));
+# - GRADIENT DESCENT does not converge at 1000 iterations
+#   I think it needs a preconditioner
+# @time opt = optimize(f, g!, h!, init_λ, GradientDescent(),
+                    # Optim.Options(show_trace=true, iterations = 200));
 
+# @time opt = optimize(f, g!, init_λ, GradientDescent(),
+                    # Optim.Options(show_trace=true, iterations = 1000));
+#%%
+
+#%%
+# # Preconditioners don't work rn
+# see: https://github.com/JuliaNLSolvers/Optim.jl/issues/763
+# but if they did ...
+# precond(n) = spdiagm(-1 => -ones(n-1), 0 => 2*ones(n), 1 => -ones(n-1))*(n+1);
 #
+# @time opt = optimize(f, g!, h!, init_λ, LBFGS(P = precond(length(init_λ))),
+#                     Optim.Options(show_trace=true, iterations = 1000));
+
+#%%
+
 #%%check results
+# final lambda
+λ = Optim.minimizer(opt);
 
 phat = compute_allocation(q, X, λ)
 phat = reshape(phat, size(A2)[1], size(pX)[1])'; # FIX - row-major reshaping - matches
@@ -263,126 +217,8 @@ Ype.MOE_lower = Ype.Y - (sqrt.(Ype.V) * 1.645);
 Ype.MOE_upper = Ype.Y + (sqrt.(Ype.V) * 1.645);
 
 # Proportion of contstraints falling outside 90% MOE
+# Init λ (0) ~60%
+# Rcpp version ~14.8%
 sum((Ype.Yhat .< Ype.MOE_lower) + (Ype.Yhat .> Ype.MOE_upper) .>= 1) / nrow(Ype)
 
 #%%
-#
-# ###########
-#%% with BlackBoxOptim
-# using BlackBoxOptim
-# #
-# res = bboptimize(f; InitCandidate = init_λ, NumDimensions = length(init_λ), MaxSteps = 10, SearchRange = (-5.,5.), method = :ProbabalisticDescent)
-#%%
-#
-# #%%
-# λf = best_candidate(res);
-#
-# neg_pe(λf)
-# #%%
-#
-# #%%check results
-# # phat = compute_allocation(q, X, zeros(length(Y_vec)))
-# phat = compute_allocation(q, X, λf) # final
-# phat = reshape(phat, size(A2)[1], size(pX)[1])'; # FIX - row-major reshaping - matches
-#
-# Yhat2 = (N * phat)' * pX
-#
-# phat_trt = (phat * N) * A1'
-# Yhat1 = phat_trt' * pX
-#
-# Yhat = vcat(vec(Yhat1), vec(Yhat2)); # FIX - matches
-#
-# Ype = DataFrame(Y = Y_vec * N, Yhat = Yhat, V = V_vec * (N^2/n))
-#
-# #90% MOEs
-# Ype.MOE_lower = Ype.Y - (sqrt.(Ype.V) * 1.645);
-# Ype.MOE_upper = Ype.Y + (sqrt.(Ype.V) * 1.645);
-#
-# # Proportion of contstraints falling outside 90% MOE
-# sum((Ype.Yhat .< Ype.MOE_lower) + (Ype.Yhat .> Ype.MOE_upper) .>= 1) / nrow(Ype)
-#
-# #%%
-#
-# ################
-#
-# ## matching pmedm.cpp
-# blah = zeros(size(X)[1]);
-# mul!(blah, -X, sparse(λ));
-# a0 = exp.(blah);
-# qXl = a0 .* q;
-#
-# p = qXl / sum(qXl);
-#
-# blah2 = zeros(size(X)[2]);
-# Xp = mul!(blah2, X', p);
-#
-# lvl = λ' * (sV * λ);
-#
-# fun = (Y_vec' * λ) + log(sum(qXl)) + (0.5 * lvl)
-#
-# grad = Y_vec + (sV * λ) - Xp
-#
-# #%%
-#
-# #%%
-# function prob(λ)
-#     blah = zeros(size(X)[1]);
-#     mul!(blah, -X, λ);
-#     a0 = exp.(blah);
-#     qXl = a0 .* q;
-#
-#     return (qXl / sum(qXl))
-# end
-# #%%
-#
-# #%%
-# function f(λ)
-#
-#     blah = zeros(size(X)[1]);
-#     mul!(blah, -X, λ);
-#     a0 = exp.(blah);
-#     qXl = a0 .* q;
-#
-#     p = qXl / sum(qXl);
-#
-#     blah2 = zeros(size(X)[2]);
-#     Xp = mul!(blah2, X', p)
-#     lvl = λ' * (sV * λ);
-#
-#     return (Y_vec' * λ) + log(sum(qXl)) + (0.5 * lvl)
-# end
-# #%%
-#
-# #%%
-# function g!(λ)
-#
-#     blah = zeros(size(X)[1]);
-#     mul!(blah, -X, λ);
-#     a0 = exp.(blah);
-#     qXl = a0 .* q;
-#
-#     p = qXl / sum(qXl);
-#
-#     blah2 = zeros(size(X)[2]);
-#     Xp = mul!(blah2, X', p);
-#
-#     return (Y_vec + (sV * λ) - Xp)
-# end
-# #%%
-#
-# #%%
-# init_λ = sparse(zeros(length(Y_vec)))
-# # p = prob(λ)
-#
-# # λ = sprand(1665, 1.)
-# #%%
-#
-# #%%
-# f(init_λ)
-# g!(init_λ)
-# #%%
-#
-# #%%
-# # optimize(f, g!, init_λ, BFGS(), Optim.Options(show_trace=true, iterations = 5))
-# # optimize(f, init_λ, BFGS(), autodiff = :forward, Optim.Options(show_trace=true, iterations = 5))
-# #%%
